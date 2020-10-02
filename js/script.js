525bce8c9c8015a7025a88d316c38cd02644a2dc
Mendjeli Asseel <asseel.mendjeli@gmail.com>
2020-10-02 17:16:46 +0200

    api fini

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 function getTopics(){
-    console.log("test");
     const f = fetch('http://bean.example.com/IngeWeb/api/index.php?url=topics');
     f.then(response => response.json()).then(data => {
-            console.log(data);
+        data.forEach(element => {
+            topics(element);
+        });
         });
 
     f.catch(function(data) { 
@@ -12,7 +13,6 @@ function getTopics(){
 function getTopicById(id){
     const f = fetch('http://bean.example.com/IngeWeb/api/index.php?url=topic/'+id);
     f.then(response => response.json()).then(data => {
-            console.log(data);
         });
 
     f.catch(function(data) { 
@@ -23,10 +23,22 @@ function getCommentbyTopic(id){
     const f = fetch('http://bean.example.com/IngeWeb/api/index.php?url=comments/'+id);
     f.then(response => response.json()).then(data => 
         {
-            console.log(data);
         });
 
     f.catch(function(data) { 
     });
 }
 
+function topics(topic){
+    console.log(topic);
+    var topics = document.getElementById("topics");
+    nouveauTexte = document.createElement("span");
+    nouveauTexte.innerHTML+='<div class="topic"> date :' + topic.date_creation +' <br> titre : '+topic.titre+' <br> contenu : '+topic.message+' </div>';
+    topics.appendChild(nouveauTexte);
+}
+
+window.onload=function(){
+        getTopics();
+        getTopicById(1);
+        getCommentbyTopic(1);
+  };
