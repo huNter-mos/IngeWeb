c708eb2e61b90bc6d7ce9bdf371317de2f6b8dd4
Mendjeli Asseel <asseel.mendjeli@gmail.com>
2020-10-02 16:31:53 +0200

    api fini

--- js/script.js
+++ js/script.js
@@ -1,44 +1,29 @@
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-}
-
-    function toTopic(topic,comment) {
-        var topics = document.getElementById("Topics");
-        nouveauTexte = document.createElement("span");
-        nouveauTexte.innerHTML+='<div> date :' + topic.date +' <br> titre : '+topic.titre+' <br> contenu : '+comment[1]+' </div>';
-        topics.appendChild(nouveauTexte);
-    }
-
-function onclickinfo(id) {
-    getcharacterById(id);
-}
-
-function characterdata(data,id) {
-    document.getElementById(id).innerHTML=data.name +'  </br> agility :'+data.agility+' </br> strength :'+data.strength+' </br> intelligence :'+data.intelligence+' </br> class :'+data.class;
-}
-
-function getPersonnage(){
-    const f = fetch('http://bean.example.com/api/characters');
-    f.then(response => response.json()).then(data => 
-        {
+function getTopics(){
+    console.log("test");
+    const f = fetch('http://bean.example.com/IngeWeb/api/index.php?url=topics');
+    f.then(response => response.json()).then(data => {
             console.log(data);
-            data.forEach((element) => {
-                toTable(element);
-            });
         });
 
     f.catch(function(data) { 
     });
 }
-function getcharacterById(id){
-    const f = fetch('http://bean.example.com/api/character/'+id);
+
+function getTopicById(id){
+    const f = fetch('http://bean.example.com/IngeWeb/api/index.php?url=topic/'+id);
+    f.then(response => response.json()).then(data => {
+            console.log(data);
+        });
+
+    f.catch(function(data) { 
+    });
+}
+
+function getCommentbyTopic(id){
+    const f = fetch('http://bean.example.com/IngeWeb/api/index.php?url=comments/'+id);
     f.then(response => response.json()).then(data => 
         {
-            characterdata(data[0],id);
+            console.log(data);
         });
 
     f.catch(function(data) { 
