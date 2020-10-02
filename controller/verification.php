b4442fc28a808812391b4963d9b96173f5dbe985
adrieng <adrien.galan@cpe.fr>
2020-10-02 17:12:55 +0200

    verification connexion 1

--- controller/verification.php
+++ controller/verification.php
@@ -1,18 +1,21 @@
 <?php
-            $msg = '';
-            
-            if (isset($_POST['login']) && !empty($_POST['username']) 
-               && !empty($_POST['password'])) {
-				
-               if ($_POST['username'] == 'tutorialspoint' && 
-                  $_POST['password'] == '1234') {
-                  $_SESSION['valid'] = true;
-                  $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
-                  
-                  echo 'You have entered valid use name and password';
-               }else {
-                  $msg = 'Wrong username or password';
-               }
-            }
-         ?>
+   $msg = 'pas de resultat';
+   if (isset($_POST['login']) && !empty($_POST['username'])
+      && !empty($_POST['password'])) {
+   
+      if ($_POST['username'] == 'tutorialspoint' && 
+         $_POST['password'] == '1234') {
+         $_SESSION['valid'] = true;
+         $_SESSION['timeout'] = time();
+         $_SESSION['username'] = 'tutorialspoint';
+         
+         $msg =  'You have entered valid use name and password';
+      }else {
+         $msg = 'Wrong username or password';
+      }
+   
+   }
+echo($msg);
+
+
+?>
