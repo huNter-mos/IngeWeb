d00d7b3840b0c13441a9dc9ebfd8a3dcec957b7e
adrieng <adrien.galan@cpe.fr>
2020-10-12 09:37:51 +0200

    changement redirection

--- api/apis/topic.php
+++ api/apis/topic.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-include 'api.php';
+require_once 'api.php';
 $data = getTopicById($_GET['api_params'][0]);
 $response = $data;
 echo json_encode($response);
