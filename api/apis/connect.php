c43691ba385b3c4ea8cadd6e9992d5f91c259f9d
adrieng <adrien.galan@cpe.fr>
2020-10-12 11:48:17 +0200

    connexion v1

--- api/apis/connect.php
+++ api/apis/connect.php
@@ -3,4 +3,4 @@ header('Content-Type: application/json');
 require_once 'api.php';
 $data = connectForum($_GET['email'],$_GET['pwd']);
 $response = $data;
-echo($response);
+echo json_encode($response);
--- controller/verification.php
+++ controller/verification.php
@@ -5,15 +5,16 @@
    if (isset($_POST['login']) && !empty($_POST['username'])
       && !empty($_POST['password'])) {
          $db = new PDO("sqlite:".__DIR__."/database.sql");
-         connectForum($_POST['username'], $_POST['password']);
-      if ($response) {
+         $data = file_get_contents("http://bean.example.com/api/index.php?url=connect&email=%22" . $_POST['username'] . "%22&pwd=%22" . $_POST['password'] . "%22");
+      if ($data[14]) {
          $_SESSION['valid'] = true;
          $_SESSION['timeout'] = time();
          $_SESSION['username'] = getUserByEmail();
          $msg =  'You have entered valid use name and password';
+         header('Location: ../view/index.php');
       }else {
          //connexion impossible
-         header( 'Location: ../view/login.html' );
+         header('Location: ../view/login.php');
       }
    
    }
